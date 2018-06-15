7b384bc21bff6c6875057cb37ed3bebb9b5d0a7f
BlackBeetleHub <kirillafendi83@gmail.com>
2018-06-15 16:55:33 +0300

    [fix] local storage

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine(
-    'postgres://tjlifzjdwsmoqh:068c8662fe033be9c78b98aae83c38c8a9aa17182458e09243a04d59c2745693@ec2-54-235-109-37.compute-1.amazonaws.com:5432/d8a6vl1bqbg0qk',
+    'sqlite:///database.db',
     convert_unicode=True)
 
 db_session = scoped_session(sessionmaker(autocommit=False,
